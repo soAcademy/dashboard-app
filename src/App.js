63cee61f4e5de02603bd0a1121a790657910624b
Panasun Sunanta <panasunsunanta@gmail.com>
2022-04-29 14:32:42 +0700

    feat: add panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar";
+import { DeathCause } from "./pages/DeathCause";
 
 const App = () => (
   <BrowserRouter>
-    <Sidebar />
+    <div className="flex">
+      <Sidebar />
+      <div className="pl-16 w-full">
+        <Routes>
+          <Route path="/death-cause" element={<DeathCause />} />
+        </Routes>
+      </div>
+    </div>
   </BrowserRouter>
 );
 export default App;
---
+++ src/pages/DeathCause/index.js
@@ -0,0 +1,62 @@
+import ThailandDeathCauseData from "../../data/thailand-death-cause.json";
+import React, { useState, useEffect } from "react";
+
+export const DeathCause = () => {
+  const [data, setData] = useState(ThailandDeathCauseData);
+  const [causeOfDeaths, setCauseOfDeaths] = useState([]);
+  const [causeOfDeathsList, setCauseOfDeathsList] = useState([]);
+  const [totalDeath, setTotalDeath] = useState(0);
+
+  useEffect(() => {
+    const _causeOfDeaths = [...new Set(data.map((row) => row.causeOfDeath))];
+    const _totalDeath = data.reduce(
+      (acc, row) => acc + row.deathMale + row.deathFemale,
+      0
+    );
+    const _causeOfDeathsList = _causeOfDeaths.map((causeOfDeath) => ({
+      causeOfDeath,
+      totalDeath: data
+        .filter((s) => s.causeOfDeath === causeOfDeath)
+        .reduce((acc, row) => acc + row.deathMale + row.deathFemale, 0),
+    }));
+
+    setCauseOfDeaths(_causeOfDeaths);
+    setCauseOfDeathsList(_causeOfDeathsList);
+    setTotalDeath(_totalDeath);
+  }, data);
+
+  const ListComponent = ({ title, number, percentage }) => (
+    <li>
+      <div className="w-full flex">
+        <div className="w-3/5">{title}</div>
+        <div className="w-2/5">
+          <div className="flex w-full text-xs text-white">
+            <div className="w-1/2 bg-blue-800 text-right pr-2">{number.toLocaleString()}</div>
+            <div className="w-1/2 bg-teal-600 pl-2">{percentage}%</div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+
+  return (
+    <div className="p-4">
+      <h1 className="font-bold text-2xl">
+        จำนวนผู้เสียชีวิต สาเหตุ และอัตราการตาย ปี 2554 - 2559 {data.length}
+      </h1>
+      <div className="w-full flex mt-4 gap-x-4">
+        <div className="border border-gray-200 w-5/12 p-2">
+          <h3 className="font-bold">สาเหตุการเสียชีวิต</h3>
+          <ul className="text-sm">
+            <ListComponent title="ทั้งหมด" number={totalDeath} percentage="100" />
+            {causeOfDeathsList.map((row) => (
+            <ListComponent title={row.causeOfDeath} number={row.totalDeath} percentage="100" />
+            ))}
+          </ul>
+        </div>
+        <div className="flex-auto">B</div>
+        <div className="bg-red-300 w-1/4">C</div>
+      </div>
+    </div>
+  );
+};
