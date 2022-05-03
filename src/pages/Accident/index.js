f636d6194f9424d7079b824f51f2c24752cd418d
Panasun Sunanta <panasunsunanta@gmail.com>
2022-05-03 17:00:35 +0700

    feat: add data

--- src/pages/Accident/index.js
+++ src/pages/Accident/index.js
@@ -3,6 +3,8 @@ import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 export const Accident = () => {
+  // const [toggleSidebar, setToggleSidebar] = useState(false);
+
   const Map = ReactMapboxGl({
     accessToken:
       "pk.eyJ1IjoicGFuYXN1biIsImEiOiJja3Jlb3VpazUwcWdwMnltNGE2eWZyemU4In0.orKVn5V00-FYfKWwSAx4mQ",
@@ -14,11 +16,19 @@ export const Accident = () => {
         style="mapbox://styles/mapbox/streets-v11"
         containerStyle={{
           height: "100vh",
-          width: "92vw",
+          width: "90vw",
         }}
         center={[100.523186, 13.736717]}
         zoom={[5]}
       ></Map>
+      <div className="bg-white font-bold fixed top-0 m-2 p-2 shadow">
+        ข้อมูลผู้เสียชีวิตจากอุบัติเหตุทางถนน
+      </div>
+      <div className="fixed top-0 right-0 h-screen flex">
+        <div className="bg-white p-2 w-48">
+          <h3 className="font-bold">สถิติ</h3>
+        </div>
+      </div>
     </div>
   );
 };
