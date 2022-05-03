b5b4fb0e288d80d1b9bab981b957b1cdbe2e464f
Panasun Sunanta <panasunsunanta@gmail.com>
2022-05-03 09:20:11 +0700

    feat: render province

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar";
 import { DeathCause } from "./pages/DeathCause";
+import { Zipcode } from "./pages/Zipcode";
 
 const App = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const App = () => (
       <div className="pl-16 w-full">
         <Routes>
           <Route path="/death-cause" element={<DeathCause />} />
+          <Route path="/zipcode" element={<Zipcode />} />
         </Routes>
       </div>
     </div>
